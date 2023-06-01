67a5d2331144d63b46fa3fc3e034e74496b45a35
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-06-01 08:46:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function update() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve("Hello, world!");
+		}, 1000);
+	});
+}
+
+update().then((text) => {
+	document.getElementById("output").textContent = text;
+});
